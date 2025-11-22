3d7cbe755394d003b8ea6e1464de8f7b99805efa
Luke Zhang <lukezhang514@outlook.com>
2025-11-22 12:27:36 -0600

    add logic for coco training process

--- code/libs/dataset.py
+++ code/libs/dataset.py
@@ -71,6 +71,37 @@ class VOCDetection(torchvision.datasets.CocoDetection):
         return img, target
 
 
+class COCODetection(torchvision.datasets.CocoDetection):
+    """
+    A wrapper for COCO dataset to format the target specific to our ConvertAnnotations transform.
+    """
+    def __init__(self, img_folder, ann_file, transforms):
+        super().__init__(img_folder, ann_file)
+        self._transforms = transforms
+        coco_ids = sorted(self.coco.getCatIds())
+        self.id_map = {coco_id: i + 1 for i, coco_id in enumerate(coco_ids)}
+
+    def __getitem__(self, idx):
+        img, target = super().__getitem__(idx)
+        image_id = self.ids[idx]
+
+        valid_annotations = []
+        for obj in target:
+            original_id = obj["category_id"]
+            if original_id in self.id_map:
+                    obj["category_id"] = self.id_map[original_id]
+                    valid_annotations.append(obj)
+            else:
+                pass
+        
+        target = dict(image_id=image_id, annotations=valid_annotations)
+        
+        if self._transforms is not None:
+            img, target = self._transforms(img, target)
+            
+        return img, target
+
+
 def build_dataset(name, split, img_folder, json_folder):
     """
     Create VOC dataset with default transforms for training / inference.
@@ -79,15 +110,19 @@ def build_dataset(name, split, img_folder, json_folder):
     if name == "VOC2007":
         assert split in ["trainval", "test"]
         is_training = split == "trainval"
+    elif name == "COCO":
+        is_training = ("train" in split)
+        ann_file = os.path.join(json_folder, f"instances_{split}.json")
+        root = os.path.join(img_folder, split)
     else:
         print("Unsupported dataset")
         return None
 
     if is_training:
         transforms = Compose([ConvertAnnotations(), 
-                              RandomHorizontalFlip(), 
                               RandomColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.1),
-                              RandomResizedCrop(size=(1024, 1024), scale=(0.1, 1.0)),
+                              RandomHorizontalFlip(), 
+                              RandomResizedCrop(size=(800, 800), scale=(0.1, 1.0)),
                               ToTensor()])
     else:
         transforms = Compose([ConvertAnnotations(), ToTensor()])
@@ -95,6 +130,10 @@ def build_dataset(name, split, img_folder, json_folder):
     if name == "VOC2007":
         dataset = VOCDetection(
             img_folder, os.path.join(json_folder, split + ".json"), transforms
+        )
+    elif name == "COCO":
+        dataset = COCODetection(
+            img_folder=root, ann_file=ann_file, transforms=transforms
         )
     return dataset
 
